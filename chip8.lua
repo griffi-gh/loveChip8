c64f7043c52b509a469795b0cee605eb0bb4bcbf
griffi-gh <45996170+griffi-gh@users.noreply.github.com>
2020-05-27 16:51:21 +0300

    Add files via upload

--- chip8.lua
+++ chip8.lua
@@ -1,12 +1,20 @@
 chip8={running=false}
 
-debug1=true
+local debug1=false --debug opcode
+local debug2=false --debug draw
 
 local function oc(C)
   return bit.band(C,0xF000)
 end
 local function ec(C)
   return bit.band(C,0x0FFF)
+end
+
+function chip8:keyDown(i)
+  key[i]=true
+end
+function chip8:keyUp(i)
+  key[i]=false
 end
 
 function chip8:stop()
@@ -20,21 +28,46 @@ function chip8:run()
   self.running=true
 end
 
+chip8.font={
+  start=0x050,
+        0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
+        0x20, 0x60, 0x20, 0x20, 0x70, -- 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, -- 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, -- 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, -- 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, -- 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, -- 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, -- 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, -- 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, -- 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, -- A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, -- B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, -- C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, -- D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, -- E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  -- F
+    }
+    
 function chip8.init()
   opcode = 0x00--current opcode (2 bytes)
   mem={} --(4096 byte)
   for i=0,0xFFF do mem[i]=0x00 end
   
   print'loading ROM...'
-  for i=0x200,0xFFF do 
-    local d=ROM[i-0x200] 
+  for i=chip8.font.start,0xFFFF do
+    local d=chip8.font[i-chip8.font.start]
+    if d==nil then break end
+    mem[i]=d
+  end
+  for i=ROM.start,0xFFFF do 
+    local d=ROM[i-ROM.start+1] 
     if d then
       mem[i]=d
     else
-      print'end'
       break
     end
   end
+  print'OK!'
   
   V={[0]=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} --cpu registers V0 V1 V2 V3 V4 V5 V6 V7 V8 V9 VA VB VC VD VE (VF -carry)
   I,pc=0x000,0x200 --Index register,program counter (start 0x200)
@@ -55,101 +88,150 @@ local function xxvv()
   local xx=bit.band(opcode,0x00FF)
   return xx,vv
 end
+local function v1v2()
+  return V[bit.brshift(bit.band(opcode,0x0F00),8)],V[bit.brshift(bit.band(opcode,0x00F0),4)]
+end
 
 function chip8.loop()
-  if mem[pc]==nil or mem[pc+1]==nil then 
-    print('[ERR]\tNo data at ',tohex(pc))
-    chip8:stop()
-    return 
-  end
-  
-  opcode = bit.bor(
-    bit.blshift(mem[pc],8),
-    mem[pc+1]
-  )
-  local s=oc(opcode)
-  
-  if debug1 and ppc~=pc then
-    ppc=pc
-    print(tohex(pc,4),tohex(opcode,4))
-    love.timer.sleep(0.2)
-  end
-  
-  if opcode==0x0000 then
-    --print('[WARN/LOW]\tNOP at',tohex(pc,3),pc)
-    pc=pc+2 --nop
-  elseif s==0x4000 then
-    local xx,vv=xxvv()
-    if xx~=vv then
-      pc=pc+4
-    else
-      pc=pc+2
-    end
-  elseif s==0x3000 then
-    local xx,vv=xxvv()
-    if xx==vv then
-      pc=pc+4
-    else
-      pc=pc+2
-    end
-  elseif s==0x5000 then
-    local v1=V[bit.brshift(bit.band(opcode,0x0F00),8)]
-    local v2=V[bit.brshift(bit.band(opcode,0x00F0),4)]
-    if v1==v2 then
-      pc=pc+4
-    else 
-      pc=pc+2
-    end
-  elseif opcode==0x00E0 then
-    chip8.cls() --clear screen
-    pc=pc+2
-  elseif s==0xA000 then
-    I=ec(opcode) --set index register
-    pc=pc+2
-  elseif s==0x1000 then
-    pc=ec(opcode)
-  elseif s==0x2000 then
-    stack[sp]=pc --call
-    sp=sp+1
-    pc=ec(opcode)
-  elseif s==0x6000 then
-    V[bit.brshift(bit.band(opcode,0x0F00),8)]=bit.band(opcode,0x00FF)
-    pc=pc+2
-  elseif opcode==0x00EE then
-    sp=sp-1 --ret
-    pc=stack[sp]
-  elseif s==0xD000 then
-    print'draw'
-    pc=pc+2
-  elseif bit.band(opcode,0xF00F)==0x8004 then
-    --adds the value of VY to VX
-    local rh,lh=bit.band(opcode,0x00F0),bit.band(opcode,0x0F00)
-    if V[bit.brshift(rh,4)] > (0xFF - V[bit.brshift(lh,8)]) then
-      V[0xF] = 1 --carry
-    else
-      V[0xF] = 0
-    end
-    local va=bit.brshift(lh,8)
-    V[va]=V[va]+V[bit.brshift(rh,4)];
-    pc=pc+2
-  elseif bit.band(opcode,0xF0FF)==0xF033 then
-    local val=V[bit.brshift(bit.band(opcode,0x0F00),8)]
-    if not val then
-      print'[ERR]\t0xFX33 no value'
+  if chip8.running then
+    if mem[pc]==nil or mem[pc+1]==nil then 
+      print('[ERR]\tNo data at ',tohex(pc))
       chip8:stop()
+      return 
+    end
+    
+    opcode = bit.bor(
+      bit.blshift(mem[pc],8),
+      mem[pc+1]
+    )
+    local s=oc(opcode)
+    
+    if debug1 and ppc~=pc then
+      ppc=pc
+      print(tohex(pc,4),tohex(opcode,4))
+      love.timer.sleep(0.2)
+    end
+    
+    if opcode==0x0000 then
+      pc=pc+2 --nop
+    elseif s==0x1000 then
+      pc=ec(opcode)
+    elseif s==0x2000 then
+      stack[sp]=pc --call
+      sp=sp+1
+      pc=ec(opcode)
+    elseif s==0x3000 then
+      local xx,vv=xxvv()
+      if xx==vv then
+        pc=pc+4
+      else
+        pc=pc+2
+      end
+    elseif s==0x4000 then
+      local xx,vv=xxvv()
+      if xx==vv then
+        pc=pc+2
+      else
+        pc=pc+4
+      end
+    elseif s==0x5000 then
+      local v1,v2=v1v2()
+      if v1==v2 then
+        pc=pc+4
+      else 
+        pc=pc+2
+      end
+    elseif s==0x6000 then
+      V[bit.brshift(bit.band(opcode,0x0F00),8)]=bit.band(opcode,0x00FF)
+      pc=pc+2
+    elseif s==0x7000 then
+      local i=bit.brshift(bit.band(opcode,0x0F00),8)
+      local val=bit.band(opcode,0x00FF)
+      V[i]=V[i]+val
+      if V[i]>0xFF then
+        V[i]=V[i]-256
+      end
+      pc=pc+2
+    elseif s==0x9000 then
+      local v1,v2=v1v2()
+      if v1==v2 then
+        pc=pc+2
+      else 
+        pc=pc+4
+      end
+    elseif s==0xD000 then --print'draw'
+      local x = V[bit.brshift(bit.band(opcode,0x0F00),8)]
+      local y = V[bit.brshift(bit.band(opcode,0x00F0),4)]
+      local h = bit.band(opcode,0x000F)
+      V[0xF]=0
+      for i=0,h-1 do --y
+        local p=mem[i+I]
+        for j=0,7 do --x
+          if bit.band(p,bit.brshift(0x80,j))~=0 then
+            local ix=x+j+(y+i)*64
+            if ix==1 then 
+              V[0xF]=1 
+            end
+            gfx[x+j+1][y+i+1]=true
+            if debug2 then print('[DRAWPIX]',x+j,y+i) end
+          end
+        end
+      end
+      pc=pc+2
+    elseif bit.band(opcode,0xF0FF)==0xF01E then
+      local ii=bit.brshift(bit.band(opcode,0x0F00),8)
+      if I+V[ii] > 0xFFF then
+        V[0xF]=1
+      else
+        V[0xF]=0
+      end
+      I=I+V[ii]
+      pc=pc+2
+    elseif opcode==0x00E0 then
+      chip8.cls() --clear screen
+      pc=pc+2
+    elseif s==0xA000 then
+      I=ec(opcode) --set index register
+      pc=pc+2
+    elseif opcode==0x00EE then
+      sp=sp-1 --ret
+      pc=stack[sp]
+    elseif bit.band(opcode,0xF00F)==0x8004 then
+      --adds the value of VY to VX
+      local rh,lh=bit.band(opcode,0x00F0),bit.band(opcode,0x0F00)
+      if V[bit.brshift(rh,4)] > (0xFF - V[bit.brshift(lh,8)]) then
+        V[0xF] = 1 --carry
+      else
+        V[0xF] = 0
+      end
+      local va=bit.brshift(lh,8)
+      V[va]=V[va]+V[bit.brshift(rh,4)];
+      pc=pc+2
+    elseif bit.band(opcode,0xF0FF)==0xF033 then
+      local val=V[bit.brshift(bit.band(opcode,0x0F00),8)]
+      if not val then
+        print'[ERR]\t0xFX33 no value'
+        chip8:stop()
+        return
+      end
+      mem[I]=val/100
+      mem[I+1]=(val/10)%10
+      mem[I+2]=(val%100)%10
+      pc=pc+2
+    elseif bit.band(opcode,0xF0FF)==0xE09E then
+      if key[V[bit.brshift(bit.band(opcode,0x0F00),8)]]==true then
+        pc=pc+4
+      else
+        pc=pc+2
+      end
+    else
+      print('[WARN/HIGH]\tUknown opcode',tohex(opcode,4),tohex(pc)) --chip8:stop()
+      pc=pc+2
       return
     end
-    mem[I]=val/100
-    mem[I+1]=(val/10)%10
-    mem[I+2]=(val%100)%10
-    pc=pc+2
-  else
-    print('[WARN/HIGH]\tUknown opcode',tohex(opcode,4),tohex(pc)) --chip8:stop()
-    pc=pc+2
-    return
+    
+    if sound_timer==1 then print'BEEP' end
+    delay_timer=math.max(0,delay_timer-1)
+    sound_timer=math.max(0,sound_timer-1)
   end
-  
-  if sound_timer==1 then print'BEEP' end
-  delay_timer=math.max(0,delay_timer-1)
-  sound_timer=math.max(0,sound_timer-1)
 end
--- main.lua
+++ main.lua
@@ -1,14 +1,34 @@
 bit=require'bitty'
 require'fn'
-require'rom'
+ROM=require'rom'
 require'chip8'
+
+local pixs=8
 
 function love.load()
   chip8:run()
 end
 
 function love.update()
-  if chip8.running then
-    chip8.loop()
+  chip8:loop()
+end
+
+function love.draw()
+  for i=1,64 do
+    for j=1,32 do
+      if gfx[i][j]==true then
+        love.graphics.rectangle('fill',(i-1)*pixs,(j-1)*pixs,pixs,pixs)
+      end
+    end
   end
+  love.graphics.print(love.timer.getFPS())
+end
+
+function love.keypressed(k)
+  local h=hextonuber(k)
+  if h then chip8:keyDown(h) end
+end
+function love.released(k)
+  local h=hextonuber(k)
+  if h then chip8:keyUp(h) end
 end
